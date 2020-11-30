85cf1762a43089689623ba335f80f81e90d04df7
kevinblade <kwangje1@gmail.com>
2020-11-30 14:32:26 +0900

    fix: code optimization

--- Going to the cinema/main.dart
+++ Going to the cinema/main.dart
@@ -1,30 +1,15 @@
 import 'dart:math';
 
-int systemA(int ticketPrice, int n) {
-  // print('systemA: ${ticketPrice * n}');
-  return ticketPrice * n;
-}
-
-int systemB(int cardPrice, int ticketPrice, double dc, int n) {
-  var initPrice = cardPrice + ticketPrice * pow(dc, 1);
-  var price = initPrice;
-  for (var i = 2; i <= n; i++) {
-    price += ticketPrice * pow(dc, i);
-  }
-  // print('systemB: ${price.ceil()}');
-  return price.ceil();
-}
-
 int movie(int cardPrice, int ticketPrice, double dc) {
   int count = 0;
-  do {
-    count += 1;
-    // print(count);
-  } while (systemB(cardPrice, ticketPrice, dc, count) >=
-      systemA(ticketPrice, count));
+  double priceB = 0.0;
+  do {} while ((priceB += (++count == 1 ? cardPrice.toDouble() : 0) +
+              ticketPrice * pow(dc, count))
+          .ceil() >=
+      (ticketPrice * count));
   return count;
 }
 
 main() {
-  print(movie(500, 15, 0.90));
+  print(movie(500, 15, 0.9));
 }
